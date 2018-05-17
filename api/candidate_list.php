c11fb68a1d00ec7214f9e5315871426310b720a4
Salil Koiral <>
2018-05-17 18:37:14 +1000

    api folder by salil

---
+++ api/candidate_list.php
@@ -0,0 +1,30 @@
+<?php
+         $dbhost = 'localhost';
+         $dbuser = 'root';
+         $dbpass = '';
+         $dbname = 'ATS';
+         $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+   
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+         }
+         $sql = 'SELECT `id`,`first_name`,`last_name` FROM `Resume_Candidate`';
+         $result = mysqli_query($conn, $sql);
+
+         // $response= array();
+         $response["success"] = false;  
+
+         if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+               
+               // $response= array();
+               $response['success']=true;
+               $response['data'][]= $row;
+            }
+         } else {
+            echo "0 results";
+         }
+
+         echo json_encode($response);
+         mysqli_close($conn);
+      ?>
---
+++ api/job_post.php
@@ -0,0 +1,32 @@
+<?php
+         $dbhost = 'localhost';
+         $dbuser = 'root';
+         $dbpass = '';
+         $dbname = 'ATS';
+         $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+         //$conn = mysqli_connect("localhost", "cybersyd_ats", "@gm3BX9wbuay", "cybersyd_ats");
+   
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+         }
+         $sql = 'SELECT jp.id,jp.Post_Title,jp.short_description,jp.Post_Description,jp.wage_per_month,jp.place_of_work,jb_p.position FROM Job_post as jp
+            Join Job_Position as jb_p on jb_p.id=jp.Job_Pos';
+         $result = mysqli_query($conn, $sql);
+
+         // $response= array();
+         $response["success"] = false;  
+
+         if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+               
+               // $response= array();
+               $response['success']=true;
+               $response['data'][]= $row;
+            }
+         } else {
+            echo "0 results";
+         }
+         header("Content-type:application/json");
+         echo json_encode($response);
+         mysqli_close($conn);
+      ?>
